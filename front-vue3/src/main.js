6966ac4e501df90a4f6ba8d366044923c0dcc4ce
Andrii Alexandru <127661933+andrii-alex@users.noreply.github.com>
2023-08-29 21:00:17 +0300

    login page

---
+++ front-vue3/src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ front-vue3/src/views/LoginView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <br>
+        <el-form :model="loginData" label-position="top">
+            <el-form-item label="Username" label-width="100px" prop="username" :rules="[
+                {
+                    required: true,
+                    message: 'Please input username!',
+                    trigger: 'blur',
+                },
+            ]">
+                <el-input v-model="loginData.username" />
+            </el-form-item>
+            <el-form-item label="Password" label-width="100px" prop="password" :rules="[
+                {
+                    required: true,
+                    message: 'Please input password!',
+                    trigger: 'blur',
+                },
+            ]">
+                <el-input v-model="loginData.password" type="password" autocomplete="off" show-password />
+            </el-form-item>
+
+            <el-row justify="end">
+                <el-form-item>
+                    <el-button size="large">Log in</el-button>
+                </el-form-item>
+            </el-row>
+        </el-form>
+    </div>
+</template>
+
+<script >
+
+export default {
+    data() {
+        return {
+            loginData: {
+                username: '',
+                password: ''
+            },
+            rules: {
+
+            }
+        }
+    }
+}
+
+</script>
